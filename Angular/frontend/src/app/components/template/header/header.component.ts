dd046e93d1791dedeb7ea49646836ed9a60970c6
wyllamcoelhoads <wyllamcoelho8@gmail.com>
2024-09-04 23:12:30 -0300

    finalizando

---
+++ Angular/frontend/src/app/components/template/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+
+export class HeaderComponent implements OnInit{
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
+
