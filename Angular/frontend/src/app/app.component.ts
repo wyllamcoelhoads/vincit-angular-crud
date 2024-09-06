b53abd668f85ac4c576e33db0322d634d353b95c
wyllamcoelhoads <wyllamcoelho8@gmail.com>
2024-09-05 22:21:11 -0300

    Adicionando o navigation e os itens

--- Angular/frontend/src/app/app.component.ts
+++ Angular/frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/template/header/header.component';  // Importe o HeaderComponent
 import { FooterComponent } from './components/template/footer/footer.component';
+import { NavigationComponent } from './components/template/navigation/navigation.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { CommonModule } from '@angular/common'; 
 import {MatIconModule} from '@angular/material/icon';
@@ -9,7 +10,7 @@ import {MatIconModule} from '@angular/material/icon';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatIconModule, RouterOutlet, HeaderComponent, FooterComponent, MatToolbarModule, CommonModule],  // Adicione o HeaderComponent aqui
+  imports: [MatIconModule, RouterOutlet, HeaderComponent, NavigationComponent, FooterComponent, MatToolbarModule, CommonModule],  // Adicione o HeaderComponent aqui
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
--- Angular/frontend/src/app/components/template/navigation/navigation.component.ts
+++ Angular/frontend/src/app/components/template/navigation/navigation.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {MatListModule} from '@angular/material/list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [MatListModule, MatSidenavModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
