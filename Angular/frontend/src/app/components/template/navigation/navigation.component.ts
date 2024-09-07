57cd12ac3c36a6d166441cf0f1146e1a8964dc04
wyllamcoelhoads <wyllamcoelho8@gmail.com>
2024-09-06 22:52:34 -0300

    Adicionando icones no navigation

--- Angular/frontend/src/app/components/template/navigation/navigation.component.ts
+++ Angular/frontend/src/app/components/template/navigation/navigation.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {MatListModule} from '@angular/material/list';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { CommonModule } from '@angular/common'; 
+import {MatIconModule} from '@angular/material/icon';
 
 
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [MatListModule, MatSidenavModule],
+  imports: [MatListModule, MatSidenavModule, CommonModule,MatIconModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
