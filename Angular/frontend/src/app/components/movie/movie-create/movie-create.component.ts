e97cf2f3843ca9c8cf534699d6f90090df7f25b0
wyllamcoelhoads <wyllamcoelho8@gmail.com>
2024-09-13 21:00:34 -0300

    adicionando formulario para filmes

--- Angular/frontend/src/app/components/movie/movie-create/movie-create.component.ts
+++ Angular/frontend/src/app/components/movie/movie-create/movie-create.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { MoviesComponent } from '../../../views/movies/movies.component';
-import { Router } from '@angular/router';
-import { MovieService } from '../movie.service';
-import { Movie } from '../movie.model';
+import {Component, OnInit } from '@angular/core';
+import {MoviesComponent } from '../../../views/movies/movies.component';
+import {Router } from '@angular/router';
+import {MovieService } from '../movie.service';
+import {Movie } from '../movie.model';
 import {MatCardModule} from '@angular/material/card';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatButtonModule } from '@angular/material/button';
+import {MatDividerModule } from '@angular/material/divider';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-movie-create',
   standalone: true,
-  imports: [MoviesComponent, MatCardModule, MatDividerModule, MatButtonModule],
+  imports: [CommonModule, FormsModule, MatIconModule, MatInputModule, MatFormFieldModule, MoviesComponent, MatCardModule, MatDividerModule, MatButtonModule],
   templateUrl: './movie-create.component.html',
   styleUrls: ['./movie-create.component.css']
 })
